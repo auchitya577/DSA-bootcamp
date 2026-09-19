45e5bdfba5577458dd4d4b6b2a20477e80099ff7
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-09-19 22:12:39 +0530

    Add files via upload

---
+++ scanner.java
@@ -0,0 +1,10 @@
+
+
+public class scanner {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+        System.out.println("You entered: " + num);
+    }
+}

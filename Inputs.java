12b846c19047a3f1d517f8e9f7d4b3213ef1f8d7
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-09-20 09:33:46 +0530

    Add files via upload

---
+++ Inputs.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        // System.out.print("please eneter some input: ");
+        // int rollno=input.nextInt();
+        // System.out.println("Your roll no is" +" " +  rollno);
+
+        // int a= 234_000_00;
+        // System.out.println(a);
+
+    //  float marks= input.nextFloat();
+    //  System.out.println(marks);
+
+    }
+}

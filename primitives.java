88e34caeefdfdaf524dab8588e3c51c9618f5bcf
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-09-20 09:10:49 +0530

    Add files via upload

---
+++ primitives.java
@@ -0,0 +1,11 @@
+public class primitives {
+    public static void main(String[] args) {
+    //     int rollno=64;
+    //     char letter='r';
+    //     float numbers=67.8f;
+    //     double largeDecimalnumbers=7878.5545;
+    //     long largeInteger=8784646464L;
+    //     boolean check=true;
+     }
+    
+}

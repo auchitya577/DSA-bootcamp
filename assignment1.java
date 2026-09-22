cdf5636e97236dd3ea2850ef27f3438f1b6b9c2e
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-09-22 21:54:38 +0530

    Add files via upload

---
+++ assignment1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class assignment1 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int a=input.nextInt();
+        if (!(a%2==0)){
+        System.out.println("number is odd");
+        } else {
+            System.out.println("number is even");
+        }
+
+    }
+    
+}

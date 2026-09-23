e71ababc96e013cb36bf3353a7bef58664597483
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-09-23 22:07:36 +0530

    Add files via upload

---
+++ Basics.java
@@ -0,0 +1,14 @@
+public class Basics {
+    public static void main(String[] args) {
+        // int a=10;
+        // if (a==10){
+        //     System.out.println("ok ok billu");
+    
+        int count=1;
+        while (count !=5){
+            System.out.println(count);   
+            count++;
+    }
+
+    }
+}

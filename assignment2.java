f33ad624692bde3fbed32f961c4e29bce50c0e69
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-09-23 22:48:44 +0530

    Add files via upload

---
+++ assignment2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class assignment2 {
+    public static void main(String[] args) {
+    Scanner in= new Scanner(System.in);
+
+    System.out.println("Enter your name: ");
+    String name= in.nextLine();
+
+    System.out.println("helloo"+name);
+
+
+    }
+    
+}

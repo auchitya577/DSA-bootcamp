4f8a6519e8fa5ea87a84f76a84efeea8c85cf99a
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-09-25 14:45:58 +0530

    Add files via upload

---
+++ Condition n Loop/loops.java
@@ -0,0 +1,57 @@
+// import java.util.Scanner;
+
+// public class loops {
+//  public static void main(String[] args) {
+//     Scanner in=new Scanner(System.in);
+
+//     // Q. PRINT NUMBERS FROM 1 TO 5
+//     /*
+//     SYNTAX FOR LOOPS:
+//     for(initialisation;condition;increment;){
+//       //body
+//     }
+//     Q print no. 1 to 5
+//     */
+// //    for (int i=1; i<=5; i+=1){
+// //     System.out.println(i);
+
+//     //   int n=in.nextInt();
+
+//     //   for (int i = 0; i <n; i++) {
+//     //     System.out.println("hello auchit");
+        
+
+//     //while loop
+//     /*
+//          while loop syntax
+//          while(condition){
+             
+//          }   
+//     */
+
+//         //  int i=1;
+//         //  while(i< 6){
+//         //     System.out.println(i);
+//         //      i+=1;          
+
+//         /*DO WHILE
+
+//         do{
+        
+//         }while(condition); */
+
+//         int n=1;
+//         do{
+//             System.out.println(n);
+//             n++;
+//         }while(n<6);
+     
+//    }
+
+
+
+
+
+
+//  }   
+

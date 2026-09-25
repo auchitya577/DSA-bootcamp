5585ba0b6e7e3ae3317f7e504e918ba0b04d3f26
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-09-25 13:41:17 +0530

    Add files via upload

---
+++ Condition n Loop/Conditionals.java
@@ -0,0 +1,23 @@
+
+
+import java.util.Scanner;
+
+public class Conditionals {
+    public static void main(String[] args) {
+
+        int salary=5000;
+
+        if (salary>10000){
+            salary+=2000;
+        }else if(salary<10000){
+            salary+=3000;
+        }else{
+            salary+=1000;
+
+            }
+            System.out.println(salary);
+        }
+        
+    }
+    
+

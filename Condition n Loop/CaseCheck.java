1c2813673dbb45341fdcd6533887a101e340564c
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-09-25 15:56:47 +0530

    Add files via upload

---
+++ Condition n Loop/CaseCheck.java
@@ -0,0 +1,28 @@
+// import java.util.Scanner;
+
+// public class CaseCheck {
+//     public static void main(String[] args) {
+//         Scanner in=new Scanner(System.in);
+
+//         // String word="Auchit";
+         
+//         // System.out.println(word.charAt(0));
+      
+//         // int a=10;
+//         // int b=20;
+
+//         // if(a!=34){
+//         //     System.out.println("hello world");
+
+
+//          char ch=in.next().trim().charAt(0);
+//          if (ch>='a' && ch<='z') {
+//             System.out.println("lower case");   
+//          }else{
+//             System.out.println("upper case");
+//         }
+      
+
+//     }
+    
+// }
